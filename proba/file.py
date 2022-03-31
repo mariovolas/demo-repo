e9896ace787202726c5947b0f405177fa2708f0e
mariovolas <93011530+mariovolas@users.noreply.github.com>
2022-03-31 12:24:31 +0200

    Create file.py

---
+++ proba/file.py
@@ -0,0 +1,24 @@
+#%%
+#3, propitati
+import random
+
+def igramo_kpm():
+    igrač = ""
+    while igrač !="e":
+        igrač = input("pritisni k za kamen,p za papir,m za makaze, šta biraš?")
+        računar = random.choice (["k" , "p" , "m"])
+        print(f"ja sam odabrao {računar}.")
+
+        if igrač == računar:
+            return "rezultat je neriješen"
+
+        if igrač_je_pobjedio(igrač,računar):
+            return "ti si pobjednik!"
+
+        return "ja sam pobijedio"
+
+# k>m, m>P  , p>k
+def igrac_je_pobjedio(igrač, računar):
+    if(igrač == "k"and računar =="m") or (igrač == "m" and računar == "p") or (igrač == "p" or računar == "k"):
+        return True
+print(igramo_kpm())
