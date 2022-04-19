d09ace7c6aa9b5912982e24aedea5a2cbe9c5c92
mariovolas <mariovolas@gmail.com>
2022-04-19 16:06:47 +0200

    dodat com laptop

--- proba/file.py
+++ proba/file.py
@@ -1,25 +1,34 @@
 #%%
-#3, propitati
-#asdasd
+# 3, propitati
+# asdasd
+# dodaj jos
 import random
+
 
 def igramo_kpm():
     igrač = ""
-    while igrač !="e":
+    while igrač != "e":
         igrač = input("pritisni k za kamen,p za papir,m za makaze, šta biraš?")
-        računar = random.choice (["k" , "p" , "m"])
+        računar = random.choice(["k", "p", "m"])
         print(f"ja sam odabrao {računar}.")
 
         if igrač == računar:
             return "rezultat je neriješen"
 
-        if igrac_je_pobjedio(igrač,računar):
+        if igrac_je_pobjedio(igrač, računar):
             return "ti si pobjednik!"
 
         return "ja sam pobijedio"
 
+
 # k>m, m>P  , p>k
 def igrac_je_pobjedio(igrač, računar):
-    if(igrač == "k"and računar =="m") or (igrač == "m" and računar == "p") or (igrač == "p" or računar == "k"):
+    if (
+        (igrač == "k" and računar == "m")
+        or (igrač == "m" and računar == "p")
+        or (igrač == "p" or računar == "k")
+    ):
         return True
+
+
 print(igramo_kpm())
